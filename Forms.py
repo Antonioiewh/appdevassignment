cc1eff67eaeefc06d9b69aadca17dff38214435e
Antonioiewh <antoniolsoderberg@gmail.com>
2024-12-31 14:48:31 +0800

    fixed validator returning error 500

--- Forms.py
+++ Forms.py
@@ -11,17 +11,17 @@ class CustomerLoginForm(Form): #Form for CustomerLogin.html
 
 
 class ListingForm(Form):
-    category = SelectField('Category', choices=[('cat1','Category 1'),('cat2','Category 2'),('cat3','Category 3'),('cat4','Category 4'),('cat5','Category 5'),validators.DataRequired()])
-    condition = RadioField('Condition',choices=[('barely_used', "Barely used"), ('frequently_used', 'Frequently used'), ('daily_used', 'Used daily'),validators.DataRequired()])
+    category = SelectField('Category',[validators.DataRequired()], choices=[('cat1','Category 1'),('cat2','Category 2'),('cat3','Category 3'),('cat4','Category 4'),('cat5','Category 5')])
+    condition = RadioField('Condition', [validators.DataRequired()], choices=[('barely_used', "Barely used"), ('frequently_used', 'Frequently used'), ('daily_used', 'Used daily')])
     title = StringField('Title',[validators.Length(min=4, max=25),validators.DataRequired()])
     description = StringField('Description',[validators.Length(min=4, max=300),validators.DataRequired()])
-    payment_method = RadioField('Payment method',choices=[('meetup','Meet-up'),('delivery','Delivery'),validators.DataRequired()]) #idk how to do this but gl to anyone trying to either
+    payment_method = RadioField('Payment method',[validators.DataRequired()],choices=[('meetup','Meet-up'),('delivery','Delivery')]) #idk how to do this but gl to anyone trying to either
 
 class uploadListingimg(Form):
     listingimg = FileField('image')
 
 
 class ReviewForm(Form):
-    rating = RadioField('Rating', choices=[(1,1),(2,2), (3,3),(4,4),(5,5),validators.DataRequired()])
+    rating = RadioField('Rating', [validators.DataRequired()],choices=[(1,1),(2,2), (3,3),(4,4),(5,5)])
     review_text = StringField('Add a comment', [validators.Length(min=4,max=1234),validators.DataRequired()])
 

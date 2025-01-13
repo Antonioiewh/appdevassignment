9b89e05ff825173d66cd8de8f6e94192d8281f15
Antonioiewh <antoniolsoderberg@gmail.com>
2025-01-13 14:42:29 +0800

    updated listing

--- __init__.py
+++ __init__.py
@@ -2661,4 +2661,4 @@ def dashboardfeedbacks():
 if __name__ == "__main__":
     app.secret_key = 'super secret key'
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.run()
+    app.run(debug=True)
